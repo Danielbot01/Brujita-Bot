66cf7c8f3821fe2924db8948ac26f29ccbae6efc
Daniel <159606732+Danielbot01@users.noreply.github.com>
2024-03-20 20:13:00 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,12 +5,14 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  const oi = *𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan};
+  let teks = `*Vamos, Vamos activos todos 🧙‍♀️✨
+
+@brujita_desing*\n\n❏ ${oi}\n\n❏ MENCIONES:\n`;
   for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
+    teks += 🔮🧙‍♀️ @${mem.id.split('@')[0]}\n;
   }
-  teks += `*└* *MONOVAN BOT *_\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += *└* *BrujiBot*_\n\n*▌│█║▌║▌║║▌║▌║▌║█*;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
